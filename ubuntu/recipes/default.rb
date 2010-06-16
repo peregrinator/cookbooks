76f209c0078913757fa48049ed84bd423a8e325f
Bob Burbach <bob.burbach@gmail.com>
2010-06-15 17:22:09 -0700

    More file organization

--- ubuntu/recipes/default.rb
+++ ubuntu/recipes/default.rb
@@ -19,17 +19,46 @@
 
 #include_recipe "apt"
 
+####################################
+#
+# Add our apt sources
+#
+####################################
 
-### Add our apt sources
+if node[:ec2]
+  template "/etc/apt/sources.list" do
+    mode 0644
+    variables :code_name => node[:lsb][:codename], :ec2_region => node[:lsb][:ec2_region]
+    notifies :run, resources(:execute => "apt-get update"), :immediately
+    source "apt/sources.list.erb"
+  end
 
-template "/etc/apt/sources.list" do
-  mode 0644
-  variables :code_name => node[:lsb][:codename], :ec2_region => node[:lsb][:ec2_region]
-  notifies :run, resources(:execute => "apt-get update"), :immediately
-  source "sources.list.erb"
+  # ec2-consistent-snapshot lives here...
+  template "/etc/apt/sources.list.d/alestic-ppa.list" do
+    mode 0644
+    variables :code_name => node[:lsb][:codename]
+    notifies :run, resources(:execute => "apt-get update"), :immediately
+    source "apt/alestic-ppa.list.erb"
+  end
+  execute "add apt signature keys for alestic ppa" do
+    command "apt-key adv --keyserver keys.gnupg.net --recv-keys BE09C571"
+    user 'root'
+  end
 end
 
-### Add libraries we'll need
+# 10-gen source for MongoDB
+template "/etc/apt/sources.list.d/mongo.list" do
+  mode 0644
+  variables :ubuntu_version => node[:platform_version]
+  notifies :run, resources(:execute => "apt-get update"), :immediately
+  source "apt/mongo.list.erb"
+end
+
+####################################
+#
+# Add libraries we'll need
+#
+####################################
 
 # nokogiri and calais gems
 package "libxml2-dev" do
@@ -53,10 +82,10 @@ end
 #
 # GROUP SETUP
 #
-###################################
+####################################
 
 group "mysql" do
-  gid 1001
+  gid 1100
 end
 
 group "deploy" do
@@ -67,13 +96,13 @@ end
 #
 # USER SETUP
 #
-###################################
+####################################
 
 ### MYSQL ###
 
 user "mysql" do
   comment "MySQL User"
-  uid "1001"
+  uid "3000"
   gid "mysql"
   not_if "cat /etc/password | grep mysql"
 end
@@ -106,35 +135,65 @@ directory "/home/deploy/.ssh" do
   not_if do File.directory?("/home/deploy/.ssh") end
 end
 
-remote_file "/home/deploy/.ssh/authorized_keys" do
-  source 'deploy_authorized_keys'
-  owner 'deploy'
-  group 'deploy'
-  mode 0600
-  action :create
-  not_if do File.exists?("/home/deploy/.ssh/authorized_keys") end
-end
-
-remote_file "/home/deploy/.ssh/known_hosts" do
-  source 'deploy_known_hosts'
-  owner 'deploy'
-  group 'deploy'
-  mode 0600
-  action :create
-  not_if do File.exists?("/home/deploy/.ssh/known_hosts") end
-end
-
-remote_file "/home/deploy/.ssh/id_rsa" do
-  source 'deploy_private_key'
-  owner 'deploy'
-  group 'deploy'
-  mode 0600
-  action :create
-  not_if do File.exists?("/home/deploy/.ssh/id_rsa") end
+%w(authorized_keys known_hosts id_rsa).each do |file|
+  remote_file "/home/deploy/.ssh/#{file}" do
+    source "users/deploy/#{file}"
+    owner 'deploy'
+    group 'deploy'
+    mode 0600
+    action :create
+    not_if do File.exists?("/home/deploy/.ssh/#{file}") end
+  end
 end
 
 # add deploy to sudoers without password required
 execute "add deploy to sudoers" do
   command "cp /etc/sudoers /etc/sudoers.bak && echo 'deploy  ALL=(ALL) NOPASSWD:ALL' | tee -a /etc/sudoers"
-  action :nothing
+end
+
+
+####################################
+#
+# CRON SETUP
+#
+####################################
+
+# our ubuntu image from EC2 has this by default for some reason...
+file "/etc/cron.d/php5" do
+  action :delete
+  only_if do File.exists?("/etc/cron.d/php5") end
+end
+
+if node[:ec2]
+  # Needed to remove old snapshots from amazon
+  # Relies on .awssecret being present in /mnt and 
+  # symlinked to /root/.awssecret
+  template "/mnt/.awssecret" do
+    variables :accesskey => node[:aws][:accesskey], :secretkey => node[:aws][:secretkey]
+    source "awssecret.erb"
+    mode 0644
+  end
+
+  link "/root/.awssecret" do
+    to "/mnt/.awssecret"
+  end
+
+  template "/etc/cron.d/ebs_backup" do
+    variables :ebs_volume_id => node[:aws][:ebs][:volume_id],
+              :mysql_user    => 'root', 
+              :mysql_passwd  => node[:mysql][:server_root_password],
+              :description   => node[:apache][:name],
+              :log           => "/mnt/backup.log"
+    source "cron/ebs_backup.erb"
+    mode 0644
+  end
+end
+
+template "/etc/cron.d/fr2_data" do
+  variables :apache_web_dir => node[:apache][:web_dir], 
+            :app_name       => node[:apache][:name], 
+            :rails_env      => node[:rails][:environment],
+            :run_user       => node[:capistrano][:deploy_user]
+  source "cron/fr2_data.erb"
+  mode 0644
 end
