1005ff05f260005cc6a1efff7d0f7f167ff1a988
Bob Burbach <bob.burbach@gmail.com>
2010-06-17 15:59:12 -0700

    Setup working varnish recipe

--- varnish/recipes/default.rb
+++ varnish/recipes/default.rb
@@ -1,8 +1,8 @@
 # Cookbook Name:: varnish
 # Recipe:: default
-# Author:: Joe Williams <joe@joetify.com>
+# Author:: Bob Burbach <Github: Peregrinator>
 #
-# Copyright 2008-2009, Joe Williams
+# Copyright 2010, Critical Juncture
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,20 +17,70 @@
 # limitations under the License.
 #
 
-package "varnish"
+# These urls were helpful in configuring Varnish properly
+# http://www.engineyard.com/blog/2010/varnish-its-not-just-for-wood-anymore/
+# http://www.all2e.com/Ressourcen/Artikel-und-Fallstudien/Varnish-installation-and-setup-on-ez-publish-based-systems
+# http://kristian.blog.linpro.no/2010/01/26/varnish-best-practices/
+# http://www.mail-archive.com/varnish-misc@varnish-cache.org/msg00336.html
 
-template "#{node[:varnish][:dir]}default.vcl" do
+group "varnish" do
+  gid 1003
+  not_if "cat /etc/group | grep varnish"
+end
+
+user "varnish" do
+  comment "Varnish User"
+  uid "4000"
+  gid "varnish"
+  not_if "cat /etc/password | grep varnish"
+end
+
+directory "#{node[:varnish][:dir]}" do
+  owner 'varnish'
+  group 'varnish'
+  action :create
+  not_if { ::File.directory?("#{node[:varnish][:dir]}") }
+end
+
+remote_file "/usr/local/src/varnish-#{node[:varnish][:version]}.tar.gz" do
+  source "#{node[:varnish][:url]}"
+  not_if { ::File.exists?("/usr/local/src/varnish-#{node[:varnish][:version]}.tar.gz") }
+end
+
+bash "Install Varnish" do
+  user 'root'
+  cwd "/usr/local/src"
+  code <<-EOH
+    tar -zxvf varnish-#{node[:varnish][:version]}.tar.gz
+    chown -R root:root /usr/local/src/varnish-#{node[:varnish][:version]}
+    cd varnish-#{node[:varnish][:version]}
+    ./configure --prefix=/usr/local --sysconfdir=/etc --localstatedir=/var/lib/ --mandir=/usr/share/man
+    make
+    make install
+    ldconfig
+  EOH
+  not_if { ::File.directory?("/usr/local/varnish-#{node[:varnish][:version]}") }
+end
+
+template "#{node[:varnish][:dir]}/default.vcl" do
   source "default.vcl.erb"
-  owner "root"
-  group "root"
+  owner "varnish"
+  group "varnish"
   mode 0644
 end
 
 template "#{node[:varnish][:default]}" do
-  source "ubuntu-default.erb"
-  owner "root"
-  group "root"
+  source "varnish.sysconfig.erb"
+  owner "varnish"
+  group "varnish"
   mode 0644
+end
+
+template "/etc/init.d/varnish" do
+  source "varnish.init.d.erb"
+  owner "varnish"
+  group "varnish"
+  mode 0755
 end
 
 service "varnish" do
@@ -38,7 +88,8 @@ service "varnish" do
   action [ :enable, :start ]
 end
 
-service "varnishlog" do
-  supports :restart => true, :reload => true
-  action [ :enable, :start ]
+directory "/var/lib/varnish/" do
+  owner "varnish"
+  group "varnish"
+  mode "0755"
 end
