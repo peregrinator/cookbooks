b6c2ce620ed43f88323807e48e897355e20b9fb3
Bob Burbach <bob.burbach@gmail.com>
2010-06-29 14:14:19 -0700

    Change set_unless to default

--- nginx/attributes/nginx.rb
+++ nginx/attributes/nginx.rb
@@ -1,6 +1,6 @@
-set_unless[:nginx][:version]      = "0.7.62"
-set_unless[:nginx][:install_path] = "/opt/nginx-#{nginx[:version]}"
-set_unless[:nginx][:src_binary]   = "#{nginx[:install_path]}/sbin/nginx"
+default[:nginx][:version]      = "0.7.62"
+default[:nginx][:install_path] = "/opt/nginx-#{nginx[:version]}"
+default[:nginx][:src_binary]   = "#{nginx[:install_path]}/sbin/nginx"
 
 case platform
 when "debian","ubuntu"
@@ -15,41 +15,43 @@ else
   set[:nginx][:binary]  = "/usr/sbin/nginx"
 end
 
-set_unless[:nginx][:configure_flags] = [
+default[:nginx][:configure_flags] = [
   "--prefix=#{nginx[:install_path]}",
   "--conf-path=#{nginx[:dir]}/nginx.conf",
   "--with-http_ssl_module",
   "--with-http_gzip_static_module"
 ]
 
-set_unless[:nginx][:listen_host] = '127.0.0.1'
-set_unless[:nginx][:listen_port] = '80'
+default[:nginx][:listen_host] = '127.0.0.1'
+default[:nginx][:listen_port] = '80'
 
-set_unless[:nginx][:gzip] = "on"
-set_unless[:nginx][:gzip_http_version] = "1.0"
-set_unless[:nginx][:gzip_comp_level] = "2"
-set_unless[:nginx][:gzip_proxied] = "any"
-set_unless[:nginx][:gzip_buffers] = '16 8k'
-set_unless[:nginx][:gzip_min_length] = 0
-set_unless[:nginx][:gzip_types] = [
+default[:nginx][:gzip] = "on"
+default[:nginx][:gzip_http_version] = "1.0"
+default[:nginx][:gzip_comp_level] = "2"
+default[:nginx][:gzip_proxied] = "any"
+default[:nginx][:gzip_buffers] = '16 8k'
+default[:nginx][:gzip_min_length] = 0
+default[:nginx][:gzip_types] = [
   "text/plain",
   "text/html",
   "text/css",
   "application/x-javascript",
+  "application/javascript",
+  "text/javascript",
   "text/xml",
   "application/xml",
-  "application/xml+rss",
-  "text/javascript"
+  "application/xml+rss"
 ]
 
-set_unless[:nginx][:keepalive]          = "on"
-set_unless[:nginx][:keepalive_timeout]  = 65
-set_unless[:nginx][:worker_processes]   = cpu[:total]
-set_unless[:nginx][:worker_connections] = 2048
-set_unless[:nginx][:server_names_hash_bucket_size] = 64
+default[:nginx][:keepalive]          = "on"
+default[:nginx][:keepalive_timeout]  = 65
+default[:nginx][:worker_processes]   = cpu[:total]
+default[:nginx][:worker_connections] = 2048
+default[:nginx][:server_names_hash_bucket_size] = 64
 
-set_unless[:nginx][:varnish_proxy] = false
-set_unless[:nginx][:varnish_proxy_host] = '127.0.0.1'
-set_unless[:nginx][:varnish_weight] = 10
-set_unless[:nginx][:varnish_max_fails] = 3
-set_unless[:nginx][:varnish_fail_timeout] = '15s'
+# default acts funny with false defaults (it overwrites true and is thus always false!)
+#default[:nginx][:varnish_proxy] = false
+default[:nginx][:varnish_proxy_host] = '127.0.0.1'
+default[:nginx][:varnish_weight] = 10
+default[:nginx][:varnish_max_fails] = 3
+default[:nginx][:varnish_fail_timeout] = '15s'
