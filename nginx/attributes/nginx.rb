8397d8c86315964781f09a73a3d95c0da2b27806
Bob Burbach <bob.burbach@gmail.com>
2010-12-24 18:50:08 -0500

    Clean up nginx to look utilize flags like ssl enabled, etc. This is part of abstracting the cookbook to utilize sites-enabled and be more modular.

--- nginx/attributes/nginx.rb
+++ nginx/attributes/nginx.rb
@@ -50,6 +50,7 @@ default[:nginx][:keepalive_timeout]  = 65
 default[:nginx][:worker_processes]   = cpu[:total]
 default[:nginx][:worker_connections] = 2048
 default[:nginx][:server_names_hash_bucket_size] = 64
+default[:nginx][:ssl_enabled]        = false
 
 default[:nginx][:logrotate][:vhost][:interval] = 'daily'
 default[:nginx][:logrotate][:vhost][:keep_for] = '7'
